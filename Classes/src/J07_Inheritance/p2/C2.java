0656bf6d1b317e1d9761636cd73e25d515489f5c
Touhidul Islam Jisan <touhidulislamjisan@gmail.com>
2019-05-18 02:01:34 +0600

    Inheritance More Example

---
+++ Classes/src/J07_Inheritance/p2/C2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package J07_Inheritance.p2;
+
+import J07_Inheritance.p1.A1;
+
+/**
+ *
+ * @author TouhidulIslam
+ */
+public class C2 extends A1{
+    public static void main(String[] args) {
+        System.out.println("publicMember : " + publicMember);
+        System.out.println("defaultMember : " + defaultMember);
+        System.out.println("protectedMember : " + protectedMember);
+        System.out.println("publicMember : " + publicMember);
+        
+    }
+}
