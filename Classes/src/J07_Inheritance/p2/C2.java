2628b835ada6982bf967b4d23ef08e7dfc2a9ef1
Touhidul Islam Jisan <touhidulislamjisan@gmail.com>
2019-05-18 05:15:43 +0600

    Inheritance More Example

--- Classes/src/J07_Inheritance/p2/C2.java
+++ Classes/src/J07_Inheritance/p2/C2.java
@@ -14,7 +14,7 @@ import J07_Inheritance.p1.A1;
 public class C2 extends A1{
     public static void main(String[] args) {
         System.out.println("publicMember : " + publicMember);
-        System.out.println("defaultMember : " + defaultMember);
+       // System.out.println("defaultMember : " + defaultMember);
         System.out.println("protectedMember : " + protectedMember);
         System.out.println("publicMember : " + publicMember);
         
