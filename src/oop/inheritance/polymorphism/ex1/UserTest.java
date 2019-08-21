66049cebe2678daf78f94bd46d5ef7b70d4f1b57
touhid-jisan <touhidulislamjisan@gmail.com>
2019-08-22 01:46:34 +0600

    Java Practice

--- src/oop/inheritance/polymorphism/ex1/UserTest.java
+++ src/oop/inheritance/polymorphism/ex1/UserTest.java
@@ -5,6 +5,15 @@ public class UserTest {
 	public void printUserType(User u) {
 		u.printUserType();
 	}
+	
+	public void approveReview(Staff s) {
+		if(s instanceof Editor) {
+			((Editor)s).approveReview();
+		}else {
+			System.out.println("Invalid Object Pass");
+		}
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -13,12 +22,35 @@ public class UserTest {
 		User editor = new Editor();
 		
 		UserTest ut = new UserTest();
+			
+		// part 1
 		
-		ut.printUserType(user);
+		/* ut.printUserType(user);
 		ut.printUserType(editor);
 		ut.printUserType(staff);
+		*/
 		
+		//part 2
+		// casting and instanceof demo
+		//ut.approveReview(new Staff());		
+		//ut.approveReview(new Editor());
 		
+		//part 3
+		/*
+		staff.postAReview("");
+		
+		((Staff) staff).printId();
+		
+		System.out.println();
+		*/
+		
+		// part 4
+		staff.staticMethod();
+		staff.instanceMethod(10);
+		
+		//part 5
+	//	staff.instanceMethod(new Staff());
+		 
 	}
 
 }
